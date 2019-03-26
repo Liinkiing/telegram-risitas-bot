586edb04a9ac5ff32a2beda1953adc0634a36fc7
Omar Jbara <omar.jbara2@gmail.com>
2019-03-26 19:56:56 +0100

    - Inlined return statement

--- src/services/RisibankClient.ts
+++ src/services/RisibankClient.ts
@@ -5,12 +5,8 @@ const BASE_URL = 'https://api.risibank.fr/api/v0'
 
 export default new class RisibankClient {
 
-  public search = async (query: string): Promise<RisibankSearchRoot> => {
-    const url = `${BASE_URL}/search?search=${encodeURI(query)}`
-    console.log(url)
-    const result = await fetch(`${url}`, { method: 'post' })
-    return result.json()
-  }
+  public search = async (query: string): Promise<RisibankSearchRoot> =>
+    (await fetch(`${BASE_URL}/search?search=${encodeURI(query)}`, {method: 'post'})).json()
 
 }
 
