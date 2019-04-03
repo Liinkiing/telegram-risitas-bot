de07049cf26506b7527ba89300def22754530552
Omar Jbara <omar.jbara2@gmail.com>
2019-04-04 01:30:42 +0200

    - Removed cache_time and keep original order from API response

--- index.ts
+++ index.ts
@@ -2,24 +2,30 @@ import * as Dotenv from 'dotenv'
 import Telegraf from 'telegraf'
 import {convertRisibankStickersToInlineResults} from "./src/utils/functions";
 import RisibankClient from "./src/services/RisibankClient";
+import { ExtraAnswerInlineQuery } from 'telegraf/typings/telegram-types';
 
 Dotenv.config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN, {username: 'Risitas'})
+
+const DEFAULT_EXTRA_OPTIONS: ExtraAnswerInlineQuery = {
+  cache_time: 0
+}
+
 bot.on("inline_query", async (ctx) => {
   try {
     const search = ctx.update.inline_query.query
     const results = await RisibankClient.search(search)
     if (results.stickers) {
-      const stickers = convertRisibankStickersToInlineResults(results.stickers).slice(0, 15)
-      return ctx.answerInlineQuery(stickers)
+      const stickers = convertRisibankStickersToInlineResults(results.stickers).slice(0,30)
+      return ctx.answerInlineQuery(stickers, DEFAULT_EXTRA_OPTIONS)
     } else {
-      return ctx.answerInlineQuery([])
+      return ctx.answerInlineQuery([], DEFAULT_EXTRA_OPTIONS)
     }
   } catch (e) {
     console.error(e)
     console.log('returning no results cause of error')
-    return ctx.answerInlineQuery([])
+    return ctx.answerInlineQuery([], DEFAULT_EXTRA_OPTIONS)
   }
 
 })
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -23,24 +23,15 @@ export const convertRisibankStickerToPhoto = (sticker: RisibankSticker): InlineP
   }
 }
 
-
-export const shuffle = <T>(array: T[]): T[] => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
 export const convertRisibankStickersToInlineResults = (stickers: RisibankSticker[]): InlineStickerResults => {
   if (!stickers) {
     return []
   }
-  return shuffle(
-    [
-      ...stickers.filter(sticker => ['jpg', 'png', 'jpeg'].includes(sticker.ext)).map(convertRisibankStickerToPhoto),
-      ...stickers.filter(sticker => ['gif'].includes(sticker.ext)).map(convertRisibankStickerToGif),
-    ]
-  )
+  return [
+    ...stickers.map(sticker => ['gif'].includes(sticker.ext) ?
+      convertRisibankStickerToGif(sticker) :
+      convertRisibankStickerToPhoto(sticker)
+    )
+  ]
 }
 
