225e2f300a0e22380679d095091645d95483e4a4
Pierre Tondereau <pierre.tondereau@protonmail.com>
2021-12-31 17:08:13 +0100

    Use express to deploy.

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as Dotenv from 'dotenv'
 import { Telegraf } from 'telegraf'
+import express, { Request, Response } from 'express'
 import { convertRisibankStickersToInlineResults } from "./src/utils/functions";
 import RisibankClient from "./src/services/RisibankClient";
 import { ExtraAnswerInlineQuery } from 'telegraf/typings/telegram-types';
@@ -29,5 +30,12 @@ bot.on("inline_query", async (ctx) => {
   }
 })
 
-bot.launch()
-console.log("Risibank bot launched!")
+const secretPath = `/telegraf/${bot.secretPathComponent()}`
+bot.telegram.setWebhook(`https://app-3fe1e446-70e0-4f03-bf2c-1827797f5812.cleverapps.io/${secretPath}`)
+
+const app = express()
+app.get('/', (req: Request, res: Response) => res.status(404).send())
+app.use(bot.webhookCallback(secretPath))
+app.listen(3000, () => {
+  console.log("Risibank bot launched on port 3000!")
+})
